b4b2d9292a486294bb4860ec24820fccad1deba3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 03:50:16 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,7 +1,7 @@
 def select_winner
   passengers = {suite_a: "Amanda Presley", suite_b: "Seymour Hoffman", suite_c: "Alfred Tennyson", suite_d: "Charlie Chaplin", suite_e: "Crumpet the Elf"}
   passengers.each do |suite, name|
-    if suite == suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       select_winner = name
     end
   end
