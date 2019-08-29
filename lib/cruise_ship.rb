86bf31ab5d11d1624d475b06965fec7522422159
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 05:18:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,13 +15,3 @@ def select_winner(passengers)
     winner
   end
 
-def happy_birthday(birthday_kids)
-  birthday_kids = {
-    "Timmy" => 9,
-    "Sarah" => 6,
-    "Amanda" => 27
-  }
-  birthday_kids.each do |kids_name, age|
-    puts "Happy brithday #{kids_name}! You are now #{age} years old!"
-  end
-end
