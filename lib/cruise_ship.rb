663a887d3a91aced31bfaffa7dda42021694e370
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 03:51:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,9 +1,9 @@
 def select_winner
   passengers = {
-    suite_a: "Amanda Presley", 
-    suite_b: "Seymour Hoffman", 
-    suite_c: "Alfred Tennyson", 
-    suite_d: "Charlie Chaplin", 
+    suite_a: "Amanda Presley",
+    suite_b: "Seymour Hoffman",
+    suite_c: "Alfred Tennyson",
+    suite_d: "Charlie Chaplin",
     suite_e: "Crumpet the Elf"
   }
   passengers.each do |suite, name|
