10930ecb3e080e1cfa7bf973ad2208dc8fca116c
Christina Gonzalez <cmgamway@gmail.com>
2019-08-29 04:47:25 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,7 +6,6 @@ def select_winner
   suite_d: "Charlie Chaplin",
   suite_e: "Crumpet the Elf"
   }
-  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner == name
